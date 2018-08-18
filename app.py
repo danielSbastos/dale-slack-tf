1382256d7d369d1d755dc4b159e2814018b39b64
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-08-18 01:09:38 -0300

    Add redis pod and svc yml and temporarly remove CircleCI and test

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
 from random import choice
 
 from flask import Flask, jsonify
+from redis import Redis
 
 
 app = Flask(__name__)
 
-GIFS_IDS = [
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+
+redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
+
+SAMPLE_GIFS_IDS = [
     "8FGM7VT9bre1TqKRds",
     "fjxMDSKaAWv45IvfHo",
     "2A6YmvLNhJXSMzBM2G",
@@ -17,9 +24,15 @@ def dale_gif():
         "response_type": "in_channel",
         "attachments": [{
             "title": "Vamo dale",
-            "image_url": "https://media.giphy.com/media/" + choice(GIFS_IDS) + "/giphy.gif"
+            "image_url": "https://media.giphy.com/media/" + choice(__get_all_gifs()) + "/giphy.gif"
         }]
     })
+
+
+def __get_all_gifs():
+    if not redis.lrange("GifsList", 0, -1):
+        redis.lpush("GifsList", *SAMPLE_GIFS_IDS)
+    return [gif.decode() for gif in redis.lrange("GifsList", 0, -1)]
 
 
 if __name__ == '__main__':
