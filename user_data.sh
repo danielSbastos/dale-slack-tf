9bc2fadaec79339809f6b9caacdfd29039e8e7e7
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-09-18 16:39:54 -0300

    Fix user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,14 +1,12 @@
-sudo yum update -y
-sudo yum install -y docker git
+sudo yum update -y && sudo yum install -y docker git
 
 git clone https://github.com/danielSbastos/dale-slack-tf.git
 cd dale-slack-tf
 
-sudo usermod -a -G docker ec2-user
-sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/bin/docker-compose > /dev/null
+sudo chmod +x /usr/bin/docker-compose
 sudo service docker start
 sudo chkconfig docker on
 
-docker-compose build
-docker-compose up
+sudo docker-compose build
+sudo docker-compose up
